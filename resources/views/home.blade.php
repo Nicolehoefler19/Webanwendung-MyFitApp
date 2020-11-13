85ab02b7a748b05af9e6552f02294a0d57bc9423
Nicolehoefler19 <57350812+Nicolehoefler19@users.noreply.github.com>
2020-11-13 10:00:53 +0100

    Home seite first version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Willkommen') }}</div>
+                <div class="card-header">{{ __('Willkommen')}} {{ Auth::user()->name }} </br> </br> {{ __('Woran möchtest du heute arbeiten?') }} </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,14 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="row">
+                        <div class="col">
+                            <a href=""><div class="bild-quader"><h2 class="subheadlines">Training</h2></div></a>
+                        </div>
+                        <div class="col">
+                            <a href=""><div class="bild-quader"><h2 class="subheadlines">Nutrition</h2></div></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
